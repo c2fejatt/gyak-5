b60175d9f711310f269b1c6183547ecff05406ce
c2fejatt <potter.atter@gmail.com>
2025-09-29 09:17:56 +0200

    mostmár eskü jó

--- program.py
+++ program.py
@@ -2,15 +2,13 @@ a = float(input("Kérem az 'a' oldalát a háromszögnek! "))
 b = float(input("Kérem az 'b' oldalát a háromszögnek! "))
 c = float(input("Kérem az 'c' oldalát a háromszögnek! "))
 
-if a + b <= c or a + c <= b or c + b <= a:
-    print("Ez nem érvényes háromszög!")
+if a + b > c or a + c > b or c + b > a:
+    print("Ez érvényes háromszög!")
 else:
     print("Ez nem érvényes háromszög!")
 
 egyikOldal = float(input("A téglalap egyik oldala: "))
-masikOldal = float(input("A téglalap másik oldala: "))
+masikOldal = float(input("A téglalap másik oldala: ")) 
 
 print(f"Téglalap területe: {egyikOldal * masikOldal}")
-print(f"Téglalap kerülete: {2 * (egyikOldal + masikOldal)}")
-
-
+print(f"Téglalap kerülete: {2 * (egyikOldal + masikOldal)}") #ez gatya
