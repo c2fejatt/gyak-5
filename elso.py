0e3fe54a5279370dc355a356548e6183a1043707
c2acsbal <c2acsbal@vasvari.org>
2025-09-29 09:27:16 +0200

    feladat javítás, külön fájlokba vétel

---
+++ elso.py
@@ -0,0 +1,8 @@
+a = float(input("Kérem az 'a' oldalát a háromszögnek! "))
+b = float(input("Kérem az 'b' oldalát a háromszögnek! "))
+c = float(input("Kérem az 'c' oldalát a háromszögnek! "))
+
+if a + b > c or a + c > b or c + b > a:
+    print("Ez érvényes háromszög!")
+else:
+    print("Ez nem érvényes háromszög!")
