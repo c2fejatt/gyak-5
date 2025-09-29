949aa4dc52431ee75a20825a8bdb6d1d34246875
c2fejatt <potter.atter@gmail.com>
2025-09-29 09:28:45 +0200

    Szétszedtük a feladatokat és megmaradt a közösített program is.

---
+++ masodik.py
@@ -0,0 +1,6 @@
+print("\nMost pedig ki fogjuk szmámolni a téglalap kerületét meg területét")
+egyikOldal = float(input("A téglalap egyik oldala: "))
+masikOldal = float(input("A téglalap másik oldala: ")) 
+
+print(f"Téglalap területe: {egyikOldal * masikOldal}")
+print(f"Téglalap kerülete: {2 * (egyikOldal + masikOldal)}")
