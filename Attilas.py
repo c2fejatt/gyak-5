880367bd7c03f39f1d2310a1f618d5329bee47a3
c2fejatt <potter.atter@gmail.com>
2025-10-13 09:30:19 +0200

    changed stuff for minister Balint

--- Attilas.py
+++ Attilas.py
@@ -1,8 +1,5 @@
 ki = input("Ki vagy? ")
-
 mit = input("Mi kell? ")
-
-
 print(f"Szoa én vagyok az {ki} és nekem kell egy {mit} majd")
-
 #ez most változás a javából
+#bocsi most cséndzseltem és mostmár rövidebb i guess
